8df6521a0ca778f86e9bed424bd774dea56953d2
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-03-07 18:16:29 -0800

    first commit

---
+++ my-practice-react-app/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App, {ExampleComponent} from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <ExampleComponent />
+  </React.StrictMode>,
+)
