37cc1a5daca2cc3667e51377eb8891d189d71db0
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-03-13 05:51:03 -0700

    another jsx file added to src directory for testing purposes

--- my-practice-react-app/src/main.jsx
+++ my-practice-react-app/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App, {ExampleComponent} from './App.jsx'
+import ExampleComponent2 from './script2.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <ExampleComponent />
+    <ExampleComponent2 />
   </React.StrictMode>,
 )
---
+++ my-practice-react-app/src/script2.jsx
@@ -0,0 +1,8 @@
+export default function ExampleComponent2(){
+  
+  return (
+      <>
+      <h1>I am a heading</h1>
+      </>
+  )
+}
